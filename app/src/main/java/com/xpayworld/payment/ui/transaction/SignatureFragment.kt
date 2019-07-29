3ab4ffeca9804d66ccbbbbaaaa1003aa88fc4686
Maryow <mario@xpay.world>
2019-07-29 09:01:24 +0800

    Applied changes in navigation identicator • applied imageview for background • attached signature UI • attach transaction history row

---
+++ app/src/main/java/com/xpayworld/payment/ui/transaction/SignatureFragment.kt
@@ -0,0 +1,19 @@
+package com.xpayworld.payment.ui.transaction
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.xpayworld.payment.R
+
+
+class SignatureFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signature, container, false)
+    }
+
+}
