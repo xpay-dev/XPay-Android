bf436bcb12630b7b1f52b27d5469bf5987671961
Maryow <mario@xpay.world>
2019-09-12 09:00:54 +0800

    [TransactionHistory]  applied BaseFragment

--- app/src/main/java/com/xpayworld/payment/ui/history/TransactionHistory.kt
+++ app/src/main/java/com/xpayworld/payment/ui/history/TransactionHistory.kt
@@ -7,14 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.xpayworld.payment.R
+import com.xpayworld.payment.ui.base.kt.BaseFragment
 
-class TransactionHistory : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction_history, container, false)
+class TransactionHistory : BaseFragment() {
+    override fun getLayout(): Int {
+        return R.layout.fragment_transaction_history
     }
 
+    override fun initView(view: View, container: ViewGroup?) {
+
+    }
 
 }
